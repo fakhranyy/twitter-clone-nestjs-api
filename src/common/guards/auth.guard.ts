6eb14b0b1abd7f28bdfa1a4f5a1a16d3d992d3df
Ziad El-Fakhrany <ziad.fakhranyy@gmail.com>
2023-12-03 14:18:00 +0200

    auth layer correction II

--- src/common/guards/auth.guard.ts
+++ src/common/guards/auth.guard.ts
@@ -1,19 +1,38 @@
 import {
   CanActivate,
   ExecutionContext,
-  HttpException,
-  HttpStatus,
   Injectable,
+  UnauthorizedException,
 } from '@nestjs/common';
-import { ExpressRequest } from 'src/common/types/expressRequest.interface';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+import { jwtConstants } from 'src/auth/constants';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  canActivate(context: ExecutionContext): boolean {
-    const request = context.switchToHttp().getRequest<ExpressRequest>();
-    if (request.user) {
-      return true;
+  constructor(private jwtService: JwtService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+    if (!token) {
+      throw new UnauthorizedException();
     }
-    throw new HttpException('Not Authorized', HttpStatus.UNAUTHORIZED);
-  } // test
+    try {
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: jwtConstants.secret,
+      });
+      // 💡 We're assigning the payload to the request object here
+      // so that we can access it in our route handlers
+      request['user'] = payload;
+    } catch {
+      throw new UnauthorizedException();
+    }
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
 }
